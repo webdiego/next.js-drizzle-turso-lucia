be9c1dce93b506143ee09e18a7592af07f6dbc19
Diego Massarni <webdiegomassarini@gmail.com>
2024-03-14 23:07:45 +1000

    Update page layout and heading text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,9 +21,9 @@ export default async function Home() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0 flex flex-col items-center justify-center">
-        <h1 className="text-5xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          The template that has it all
+      <div className="p-4 max-w-5xl mx-auto relative z-10 w-full pt-20 md:pt-0 flex flex-col items-center justify-center">
+        <h1 className="text-5xl lg:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          The template for building fullstack applications
         </h1>
 
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
